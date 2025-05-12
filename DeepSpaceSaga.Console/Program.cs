715e97d89ffceba1c6499a8aa93b6eca2d7ebcc9
Vitaly.Sushilov <vitaly.s@lsports.eu>
2025-05-12 10:52:35 +0300

    Add Console application

--- DeepSpaceSaga.Console/Program.cs
+++ DeepSpaceSaga.Console/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DeepSpaceSaga.Controller;
+using DeepSpaceSaga.Server;
+using log4net;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+namespace DeepSpaceSaga.Console
+{
+    internal static class Program
+    {
+        public static IServiceProvider? ServiceProvider { get; private set; }
+        
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+        
+       [STAThread]
+       static void Main()
+       {
+           // Create Logs directory if it doesn't exist
+           Directory.CreateDirectory("Logs");
+           
+           // Configure log4net
+           var configFile = new FileInfo("log4net.config");
+           log4net.Config.XmlConfigurator.Configure(configFile);
+
+           Logger.Info("Start 'Deep Space Saga' game desktop client.");
+
+           ServiceProvider = CreateHostBuilder().Build().Services;
+           
+           System.Console.WriteLine("Hello, World!");
+       }
+       
+       static IHostBuilder CreateHostBuilder()
+       {
+           return Host.CreateDefaultBuilder()
+               .ConfigureServices((_, services) => {
+                   services.AddClientControls();
+                   services.AddControllerServices();
+                   services.AddServerServices();
+               });
+       }
+    }
+
+}
+
---
+++ DeepSpaceSaga.Console/StartupExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeepSpaceSaga.Console
+{
+    [ExcludeFromCodeCoverage]
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddClientControls(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}

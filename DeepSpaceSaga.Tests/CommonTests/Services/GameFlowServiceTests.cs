51c7844f62eaa56d3dfb86678a7b291193a98046
SuhilovVitaly <98092169+SuhilovVitaly@users.noreply.github.com>
2025-05-13 15:17:02 +0300

    Add tests to services

---
+++ DeepSpaceSaga.Tests/CommonTests/Services/GameFlowServiceTests.cs
@@ -0,0 +1,113 @@
+using DeepSpaceSaga.Common.Abstractions.Services;
+using DeepSpaceSaga.Common.Abstractions.Session.Entities;
+using DeepSpaceSaga.Common.Implementation.Services;
+using DeepSpaceSaga.Common.Implementation.GameLoop;
+using Moq;
+using Xunit;
+
+namespace DeepSpaceSaga.Tests.CommonTests.Services;
+
+public class GameFlowServiceTests
+{
+    private readonly Mock<ISessionInfo> _sessionInfoMock;
+    private readonly Mock<IExecutor> _executorMock;
+    private readonly GameFlowService _sut;
+
+    public GameFlowServiceTests()
+    {
+        _sessionInfoMock = new Mock<ISessionInfo>();
+        _executorMock = new Mock<IExecutor>();
+        _sut = new GameFlowService(_sessionInfoMock.Object, _executorMock.Object);
+    }
+
+    [Fact]
+    public void Constructor_InitializesCorrectly()
+    {
+        // Assert
+        Assert.Equal(_sessionInfoMock.Object, _sut.SessionInfo);
+    }
+
+    [Fact]
+    public void SessionStart_CallsExecutorStart()
+    {
+        // Act
+        _sut.SessionStart();
+
+        // Assert
+        _executorMock.Verify(x => x.Start(It.IsAny<Action<ISessionInfo, CalculationType>>()), Times.Once);
+    }
+
+    [Fact]
+    public void SessionPause_CallsExecutorStop()
+    {
+        // Act
+        _sut.SessionPause();
+
+        // Assert
+        _executorMock.Verify(x => x.Stop(), Times.Once);
+    }
+
+    [Fact]
+    public void SessionResume_CallsExecutorResume()
+    {
+        // Act
+        _sut.SessionResume();
+
+        // Assert
+        _executorMock.Verify(x => x.Resume(), Times.Once);
+    }
+
+    [Fact]
+    public void SessionStop_CallsExecutorStop()
+    {
+        // Act
+        _sut.SessionStop();
+
+        // Assert
+        _executorMock.Verify(x => x.Stop(), Times.Once);
+    }
+
+    [Fact]
+    public void TurnExecution_CanBeSetAndExecuted()
+    {
+        // Arrange
+        var wasExecuted = false;
+        _sut.TurnExecution = (_, _) => wasExecuted = true;
+
+        // Act
+        _sut.TurnExecution(_sessionInfoMock.Object, CalculationType.Turn);
+
+        // Assert
+        Assert.True(wasExecuted);
+    }
+
+    [Fact]
+    public void SessionStart_WithNullTurnExecution_StartsExecutorWithoutException()
+    {
+        // Arrange
+        _sut.TurnExecution = null;
+
+        // Act & Assert
+        var exception = Record.Exception(() => _sut.SessionStart());
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void SessionStart_PassesCorrectDelegateToExecutor()
+    {
+        // Arrange
+        Action<ISessionInfo, CalculationType> capturedDelegate = null;
+        _executorMock.Setup(x => x.Start(It.IsAny<Action<ISessionInfo, CalculationType>>()))
+            .Callback<Action<ISessionInfo, CalculationType>>(d => capturedDelegate = d);
+
+        var wasExecuted = false;
+        _sut.TurnExecution = (_, _) => wasExecuted = true;
+
+        // Act
+        _sut.SessionStart();
+        capturedDelegate?.Invoke(_sessionInfoMock.Object, CalculationType.Turn);
+
+        // Assert
+        Assert.True(wasExecuted);
+    }
+} 
--- DeepSpaceSaga.Tests/ServerTests/LocalGameServerTests.cs
+++ DeepSpaceSaga.Tests/ServerTests/LocalGameServerTests.cs
@@ -1,16 +1,115 @@
 using DeepSpaceSaga.Common.Implementation.Services;
+using DeepSpaceSaga.Common.Implementation.GameLoop;
+using DeepSpaceSaga.Server;
+using DeepSpaceSaga.Common.Abstractions.Session.Entities;
+using Xunit;
 
 namespace DeepSpaceSaga.Tests.ServerTests;
 
 public class LocalGameServerTests
 {
     private readonly LocalGameServer _sut;
+    private readonly IGameFlowService _gameFlowService;
+    private readonly SessionInfo _sessionInfo;
+    private readonly Executor _executor;
+    private GameSessionDTO? _lastExecutedSession;
 
     public LocalGameServerTests()
     {
         // Initialize the logger
         TestLoggerRepository.Initialize();
-        _sut = new LocalGameServer(new GameFlowService(new SessionInfo(), new Executor( new SessionInfo())));
+        
+        // Setup dependencies
+        _sessionInfo = new SessionInfo();
+        _executor = new Executor(_sessionInfo);
+        _gameFlowService = new GameFlowService(_sessionInfo, _executor);
+        _sut = new LocalGameServer(_gameFlowService);
+        
+        // Subscribe to OnTurnExecute event
+        _sut.OnTurnExecute += session => _lastExecutedSession = session;
     }
-    
+
+    [Fact]
+    public void SessionStart_ShouldStartGameFlow()
+    {
+        // Act
+        _sut.SessionStart();
+
+        // Assert
+        Assert.Equal(SessionState.NotStarted, _sessionInfo.State);
+        Assert.False(_sessionInfo.IsPaused);
+    }
+
+    [Fact]
+    public void SessionPause_ShouldPauseGameFlow()
+    {
+        // Arrange
+        _sut.SessionStart();
+
+        // Act
+        _sut.SessionPause();
+
+        // Assert
+        Assert.True(_sessionInfo.IsPaused);
+    }
+
+    [Fact]
+    public void SessionResume_ShouldResumeGameFlow()
+    {
+        // Arrange
+        _sut.SessionStart();
+        _sut.SessionPause();
+
+        // Act
+        _sut.SessionResume();
+
+        // Assert
+        Assert.False(_sessionInfo.IsPaused);
+    }
+
+    [Fact]
+    public void SessionStop_ShouldStopGameFlow()
+    {
+        // Arrange
+        _sut.SessionStart();
+
+        // Act
+        _sut.SessionStop();
+
+        // Assert
+        Assert.True(_sessionInfo.IsPaused);
+    }
+
+    [Fact]
+    public void TurnExecution_ShouldIncrementTurnAndNotifySubscribers()
+    {
+        // Arrange
+        var initialTurn = _sessionInfo.Turn;
+
+        // Act
+        _sut.TurnExecution(_sessionInfo, CalculationType.Turn);
+
+        // Assert
+        Assert.NotNull(_lastExecutedSession);
+        Assert.NotEqual(Guid.Empty, _lastExecutedSession.Id);
+        Assert.Equal(initialTurn + 1, _lastExecutedSession.Turn);
+        Assert.NotNull(_lastExecutedSession.SpaceMap);
+    }
+
+    [Theory]
+    [InlineData(CalculationType.Tick)]
+    [InlineData(CalculationType.Turn)]
+    [InlineData(CalculationType.Cycle)]
+    public void TurnExecution_DifferentTypes_ShouldIncrementTurn(CalculationType type)
+    {
+        // Arrange
+        var initialTurn = _sessionInfo.Turn;
+
+        // Act
+        _sut.TurnExecution(_sessionInfo, type);
+
+        // Assert
+        Assert.NotNull(_lastExecutedSession);
+        Assert.Equal(initialTurn + 1, _lastExecutedSession.Turn);
+    }
 } 
